1f61e9ddf8f933707a1273ac43aff2b2d0f73c12
sulavchhetri <sulavthapachhetri@gmail.com>
2024-11-03 13:46:03 +0545

    created modal for updating and creating new invoices

--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -1,104 +1,124 @@
 <template>
-  <div class="table-top">
-    <div class="table-header">
-      <div class="table-header-item">Task</div>
-      <div class="table-header-item">Man-hours</div>
-      <div class="table-header-item">Unit Price</div>
-      <div class="table-header-item">Discount</div>
-      <div class="table-header-item">Amount</div>
-      <div class="table-header-item">Action</div>
+  <div class="invoice-handler">
+    <div class="invoice-header">
+      <h1>Invoice Handler</h1>
+      <button class="create-invoice-btn" @click="openModal">New +</button>
     </div>
-    <div class="children-container">
-      <TaskRow
-        v-for="(row, index) in tableData"
-        :key="index"
-        :table-id="index"
-        :root="tableData"
-        :table-data="row"
-        @refresh-table-data="refreshTableData"
-      />
+    <div class="table-top">
+      <div class="table-header">
+        <div class="table-header-item">Task</div>
+        <div class="table-header-item">Man-hours</div>
+        <div class="table-header-item">Unit Price</div>
+        <div class="table-header-item">Discount</div>
+        <div class="table-header-item">Amount</div>
+        <div class="table-header-item">Action</div>
+      </div>
+      <div class="children-container">
+        <TaskRow
+          v-for="(row, index) in tableData"
+          :key="index"
+          :table-id="index"
+          :root="tableData"
+          :table-data="row"
+          @refresh-table-data="refreshTableData"
+        />
+      </div>
     </div>
+    <InvoiceModal
+      v-if="showInvoiceModal"
+      :update-mode="false"
+      :task-key="getNextRootKey()"
+      @close-invoice-modal="handleInvoiceModal"
+    />
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted, watch } from "vue";
 import TaskRow from "./TaskRow.vue";
+import InvoiceModal from "./InvoiceModal.vue";
 import { dragStore } from "../utils.js";
 import { calcStore } from "../calculator/calc";
 
 const tableData = ref({
   1: {
-    Task: "Task 1",
-    "Man-hours": 40,
-    UnitPrice: 150,
-    Discount: 20,
-    Amount: 3000,
+    task: "Task 1",
+    hours: 40,
+    unit_price: 150,
+    discount: 20,
+    amount: 3000,
     1.1: {
-      Task: "Task 1.1",
-      "Man-hours": 60,
-      UnitPrice: 20,
-      Discount: 10,
-      Amount: 2000,
+      task: "Task 1.1",
+      hours: 60,
+      unit_price: 20,
+      discount: 10,
+      amount: 2000,
     },
     1.2: {
-      Task: "Task 1.2",
-      "Man-hours": 120,
-      UnitPrice: 30,
-      Discount: 50,
-      Amount: 1000,
+      task: "Task 1.2",
+      hours: 120,
+      unit_price: 30,
+      discount: 50,
+      amount: 1000,
     },
   },
   2: {
-    Task: "Task 2",
-    "Man-hours": 30,
-    UnitPrice: 300,
-    Discount: 25,
-    Amount: 500,
+    task: "Task 2",
+    hours: 30,
+    unit_price: 300,
+    discount: 25,
+    amount: 500,
   },
   3: {
-    Task: "Task 3",
-    "Man-hours": 100,
-    UnitPrice: 25,
-    Discount: 15,
-    Amount: 100,
+    task: "Task 3",
+    hours: 100,
+    unit_price: 25,
+    discount: 15,
+    amount: 100,
   },
   4: {
-    Task: "Task 4",
-    "Man-hours": 8,
-    UnitPrice: 600,
-    Discount: 1000,
-    Amount: 1500,
+    task: "Task 4",
+    hours: 8,
+    unit_price: 600,
+    discount: 1000,
+    amount: 1500,
   },
   5: {
-    Task: "Task 5",
-    "Man-hours": 100,
-    UnitPrice: 500,
-    Discount: 2500,
-    Amount: 2000,
+    task: "Task 5",
+    hours: 100,
+    unit_price: 500,
+    discount: 2500,
+    amount: 2000,
   },
   6: {
-    Task: "Task 6",
-    "Man-hours": 60,
-    UnitPrice: 150,
-    Discount: 300,
-    Amount: 3000,
+    task: "Task 6",
+    hours: 60,
+    unit_price: 150,
+    discount: 300,
+    amount: 3000,
     6.1: {
-      Task: "Task 6.1",
-      "Man-hours": 70,
-      UnitPrice: 150,
-      Discount: 300,
-      Amount: 2000,
+      task: "Task 6.1",
+      hours: 70,
+      unit_price: 150,
+      discount: 300,
+      amount: 2000,
       "6.1.1": {
-        Task: "Task 6.1.1",
-        "Man-hours": 50,
-        UnitPrice: 150,
-        Discount: 200,
-        Amount: 1000,
+        task: "Task 6.1.1",
+        hours: 50,
+        unit_price: 150,
+        discount: 200,
+        amount: 1000,
       },
     },
   },
 });
+
+
+const showInvoiceModal = ref(false);
+
+const openModal = () => {
+  showInvoiceModal.value = true;
+};
 
 const refreshTableData = (data) => {
   dragStore.clearStore();
@@ -122,9 +142,54 @@ watch(
   },
   { deep: true }
 );
+
+const handleInvoiceModal = (data)=> {
+  showInvoiceModal.value = false;
+  console.log(data, "came here")
+}
+
+const getNextRootKey = () => {
+  const keys = Object.keys(tableData.value).map(Number);
+  const maxKey = Math.max(...keys);
+  return (maxKey + 1).toString();
+}
+
 </script>
 
 <style scoped>
+.invoice-handler {
+  margin: 10px;
+}
+
+.invoice-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 40px;
+  background-color: #4b586e; /* Slightly lighter than #36454f */
+  color: white;
+  border-radius: 6px;
+}
+
+.invoice-header h1 {
+  margin: 0;
+  font-size: 24px;
+}
+
+.create-invoice-btn {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.create-invoice-btn:hover {
+  background-color: #218838; /* Darker green on hover */
+}
+
 .table-top {
   padding: 5px;
   font-family: Arial, Helvetica, sans-serif;
